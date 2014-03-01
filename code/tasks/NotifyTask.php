bab379730bfca2ab4ce6be062cdb10d981202c25
Stig Lindqvist <stojg.lindqvist@gmail.com>
2014-03-01 14:03:48 +1300

    Adding title, description and better help messages

--- code/tasks/NotifyTask.php
+++ code/tasks/NotifyTask.php
@@ -7,6 +7,16 @@
 class NotifyTask extends BuildTask {
 	
 	/**
+	 * @var string $title Shown in the overview on the {@link TaskRunner} HTML or CLI interface. Should be short and concise, no HTML allowed.
+	 */
+	protected $title = 'Notify user';
+	
+	/**
+	 * @var string $description Describe the implications the task has, and the changes it makes. Accepts HTML formatting.
+	 */
+	protected $description = 'Send a message to a user';
+	
+	/**
 	 * 
 	 * @param SS_HTTPRequest $request
 	 */
@@ -15,24 +25,24 @@ class NotifyTask extends BuildTask {
 		$message = trim($request->requestVar('message'));
 		
 		if(!$memberEmail) {
-			echo 'Please provide an email'.PHP_EOL;
+			echo 'Please provide an email, eg ?email=user@example.com'.PHP_EOL;
 			exit(1);
 		}
 		
 		$members = Member::get()->filter('Email', $memberEmail);
 		if(!$members->count()) {
-			echo 'Please provide a valid email'.PHP_EOL;
+			echo 'Please provide an existing member email'.PHP_EOL;
 			exit(1);
 		}
 		
 		$member = $members->first();
 		
 		if(!$message) {
-			echo 'Please provide a message'.PHP_EOL;
+			echo 'Please provide a message, eg ?message=hello'.PHP_EOL;
 			exit(1);
 		}
 		
-		Notification::notify($member, $message);
+		TimelineEvent::notify($member, $message);
 		
 		echo 'Member '.$member->Email.' has been notified'.PHP_EOL;
 	}
